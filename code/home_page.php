307b541a503ecfe12e68e063ea0bab05821e8cee
Dhiko <dhikojjp@gmail.com>
2020-02-24 19:46:22 +0700

    change page

--- code/home_page.php
+++ code/home_page.php
@@ -4,7 +4,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="css/home_page.css" type="text/css">
+    <link rel="stylesheet" href="../css/home_page.css" type="text/css">
 </head>
 
 <body>
@@ -14,7 +14,7 @@
     </div>
 
     <div class="row">
-        <div class="col-3 menu">
+        <div class="col-3 col-s-3 menu">
             <ul>
                 <li>The Flight</li>
                 <li>The City</li>
@@ -23,11 +23,34 @@
             </ul>
         </div>
 
-        <div class="col-9">
-            <h1>The City</h1>
-            <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
-            <p>Resize the browser window to see how the content respond to the resizing.</p>
+        <div class="col-9 col-s-9 content">
+
+            <div class="col-12 col-s-12 content_satu">
+                <div class="col-3 col-s-3 nilai_akhir">
+                    <p>Nilai Akhir</p>
+                    <h2>C</h2>
+                </div>
+
+                <div class="col-3 col-s-3 semester">
+                    <p>Semester</p>
+                    <h2>7</h2>
+                </div>
+            </div>
+
+
+            <div class="col-12 col-s-12 content_dua">
+                <div class="col-3 col-s-3 info_kp">
+                    <p>Info Kerja Praktik</p>
+                    <h2>Ini Info Tentang Kerja Praktik</h2>
+                </div>
+            </div>
+
         </div>
+
+    </div>
+
+    <div class="footer">
+        <p>Resize the browser window to see how the content respond to the resizing.</p>
     </div>
 
 </body>
