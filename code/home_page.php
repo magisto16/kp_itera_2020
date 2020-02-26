4f76d9715fbd6d81ec169c378fa0dcbda6796f18
Dhiko <dhikojjp@gmail.com>
2020-02-26 16:05:49 +0700

    custom navbar

--- code/home_page.php
+++ code/home_page.php
@@ -5,18 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="../css/home_page.css" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
 
     <div class=" header">
-        <h1 class="col-9 col-s-12">Institut Teknologi Sumatera</h1>
+        <h1>Institut Teknologi Sumatera</h1>
+    </div>
 
-        <div class="col-3 col-s-12 head-content">
-            <p>Admin</p>
-            <a href="#">Logout</a>
-        </div>
+    <div class="nav-bar">
+        <nav class="navbar navbar-custom navbar-static-top">
+            <div class="container-fluid">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Dhiko JangJaya Putra</a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+                </ul>
+            </div>
+        </nav>
     </div>
 
     <div class="row">
