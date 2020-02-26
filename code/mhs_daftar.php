74d1d5f9992f61dda0e34836ec13a93f5c1ee777
Dhiko <dhikojjp@gmail.com>
2020-02-26 16:56:59 +0700

    page daftar

---
+++ code/mhs_daftar.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="../css/home_page.css" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+    <div class=" header">
+        <h1>Institut Teknologi Sumatera</h1>
+    </div>
+
+    <div class="nav-bar">
+        <nav class="navbar navbar-custom navbar-static-top">
+            <div class="container-fluid">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Dhiko JangJaya Putra</a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+    <div class="row">
+        <div class="col-3 col-s-3 menu">
+            <ul>
+                <li>Beranda</li>
+                <li>Daftar</li>
+                <li>Referensi</li>
+                <li>Unduh Form</li>
+                <li>Upload</li>
+                <li>Pengajuan Seminar</li>
+                <li>Log Sheet</li>
+            </ul>
+        </div>
+
+        <div class="col-9 col-s-9 content">
+
+            <form class="col-12 col-s-12 form" action="#">
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                </div>
+                <div class="form-group">
+                    <label for="pwd">Password:</label>
+                    <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+                </div>
+                <div class="form-group form-check">
+                    <label class="form-check-label">
+                        <input class="form-check-input" type="checkbox" name="remember"> Remember me
+                    </label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+
+        </div>
+
+    </div>
+
+    <div class="footer">
+        <p><i class="fa fa-copyright" aria-hidden="true"></i> Copyright</p>
+    </div>
+
+</body>
+
+</html>
