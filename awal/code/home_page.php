d309f2db114db1e2b25c1bff46e882fb415cb539
Dhiko <dhikojjp@gmail.com>
2020-03-04 11:01:35 +0700

    tampilan awal

--- awal/code/home_page.php
+++ awal/code/home_page.php
@@ -13,63 +13,31 @@
 
 <body>
 
-    <div class=" header">
+    <div class="header">
         <h1>Institut Teknologi Sumatera</h1>
     </div>
 
-    <div class="nav-bar">
-        <nav class="navbar navbar-custom navbar-static-top">
-            <div class="container-fluid">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Dhiko JangJaya Putra</a></li>
-                    <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
-                </ul>
-            </div>
-        </nav>
-    </div>
-
     <div class="row">
-        <div class="col-3 col-s-12 menu">
-            <ul>
-                <li><a href="home_page.php">Beranda</a></li>
-                <li><a href="mhs_daftar.php">Daftar</a></li>
-                <li><a href="mhs_referensi.php">Referensi</a></li>
-                <li><a href="mhs_unduh.php">Unduh Form</a></li>
-                <li><a href="mhs_upload.php">Upload</a></li>
-                <li><a href="mhs_pengajuan.php">Pengajuan Seminar</a></li>
-            </ul>
+        <div class="col-6 col-s-12 menu">
+            <h4>Website Resmi Kerja Praktik Teknik Informatika
+                Institut Teknologi Sumatera</h4>
+            <button type="button"><a href="../../authentification/login/code/login.php">Login</a></button>
+            <button type="button"><a href="../../authentification/register/code/register.php">Register</a></button>
         </div>
 
-        <div class="col-9 col-s-12 content">
+        <div class="col-6 col-s-12 content">
 
             <div class="col-9 col-12 content_satu">
                 <div class="col-3 col-s-12 semester">
-                    <p>Semester</p>
-                    <h2>7</h2>
-                </div>
-
-                <div class="col-3 col-s-12 catatan">
-                    <p>Catatan Dosen Pembimbing</p>
-                    <a href="#">Lihat</a>
-                </div>
-            </div>
-
-
-            <div class="col-12 col-s-12 content_dua">
-                <div class="col-12 col-s-12 info_kp">
-                    <p>Info Kerja Praktik</p>
-                    <h2>Ini Info Tentang Kerja Praktik</h2>
-                </div>
-            </div>
-
-            <div class="col-12 col-s-12 content_tiga">
-                <div class="col-12 col-s-12 info_kp">
-                    <p>Info Fotonya</p>
-                    <h2>Ini Info Tentang Foto-Fotonya</h2>
+                    <p>Foto-Foto Kerja Praktik</p>
                 </div>
             </div>
         </div>
+    </div>
 
+    <div class="info_kp">
+        <p>Info Kerja Praktik</p>
+        <h2>Ini Info Tentang Kerja Praktik</h2>
     </div>
 
     <div class="footer">
