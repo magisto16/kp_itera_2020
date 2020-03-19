4f84c1a89c9471628ae1dfcd6c74e055bf7b87fb
Dhiko <dhikojjp@gmail.com>
2020-03-19 08:31:05 +0700

    mhs_pengajuan

--- mahasiswa/code/mhs_pengajuan.php
+++ mahasiswa/code/mhs_pengajuan.php
@@ -44,18 +44,30 @@
 
             <form class="col-12 col-s-12 form" action="#">
                 <div class="form-group">
-                    <label for="email">Email:</label>
-                    <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                    <label for="nama">Nama:</label>
+                    <input type="text" class="form-control" id="nama" placeholder="Masukkan Nama" name="nama">
                 </div>
                 <div class="form-group">
-                    <label for="pwd">Password:</label>
-                    <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+                    <label for="nim">NIM:</label>
+                    <input type="text" class="form-control" id="nim" placeholder="Masukkan NIM" name="nim">
                 </div>
-                <div class="form-group form-check">
-                    <label class="form-check-label">
-                        <input class="form-check-input" type="checkbox" name="remember"> Remember me
-                    </label>
+                <div class="form-group">
+                    <label for="tmpt">Tempat Kerja Praktik:</label>
+                    <input type="text" class="form-control" id="tmpt" placeholder="Masukkan Tempat Kerja Praktik" name="tmpt">
                 </div>
+                <div class="form-group">
+                    <label for="waktu">Waktu Kerja Praktik:</label>
+                    <input type="text" class="form-control" id="waktu" placeholder="Masukkan Waktu Kerja Praktik" name="waktu">
+                </div>
+                <div class="form-group">
+                    <label for="waktu_seminar">Waktu Seminar:</label>
+                    <input type="text" class="form-control" id="waktu_seminar" placeholder="Masukkan Waktu Seminar" name="waktu_seminar">
+                </div>
+                <div class="form-group">
+                    <label for="tempat_seminar">Tempat Seminar:</label>
+                    <input type="text" class="form-control" id="tempat_seminar" placeholder="Masukkan Tempat Seminar" name="tempat_seminar">
+                </div>
+
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
 
